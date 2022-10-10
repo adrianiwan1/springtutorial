88e444a4b42267a42f939756ad5d6748d40787ad
Adrian Iwañski <adriadniwan1@gmail.com>
2022-10-10 20:44:31 +0200

    Initial commit

---
+++ src/main/java/pl/springtutorial/springtutorial/HelloWorldController.java
@@ -0,0 +1,19 @@
+package pl.springtutorial.springtutorial;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping
+    public String helloworld() {
+        return "hello World <br> " +
+                "<a href=\"/goodbye\">goodbye</a>";
+    }
+
+    @RequestMapping("goodbye")
+    public String goodbye(){
+        return "Goodbye 1st site <br>" +
+                "<a href=\"/\">welcome</a>";
+    }
+}
