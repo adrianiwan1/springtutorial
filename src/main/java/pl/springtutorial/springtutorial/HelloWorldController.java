01e0971852bd32db0556db2f53e2cb975f652013
adrianiwan1 <adrianiwan1@gmail.com>
2022-10-12 15:11:54 +0200

    SimpleCalculate

--- src/main/java/pl/springtutorial/springtutorial/HelloWorldController.java
+++ src/main/java/pl/springtutorial/springtutorial/HelloWorldController.java
@@ -1,11 +1,19 @@
 package pl.springtutorial.springtutorial;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping
+
+
+    @RequestMapping("/metoda") public String widok(@RequestParam(value ="a", required = false,defaultValue="0") Integer danaA, @RequestParam(value = "b", required = false, defaultValue = "0") Integer danaB)
+    {
+        int i = danaB + danaA;
+        return "wynik to" + i;
+    }
+
     public String helloworld() {
         return "hello World <br> " +
                 "<a href=\"/goodbye\">goodbye</a>";
@@ -13,7 +21,7 @@ public class HelloWorldController {
 
     @RequestMapping("goodbye")
     public String goodbye(){
-        return "Goodbye from 1st site ;) <br>" +
+        return "Goodbye from 1st site ;( <br>" +
                 "<a href=\"/\">welcome</a>";
     }
 }
