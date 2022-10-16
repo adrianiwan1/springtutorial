f91513ce7bc6bc7d0295e321e1e08965feca7bd2
Adrian Iwañski <adriadniwan1@gmail.com>
2022-10-17 00:20:18 +0200

    Simple loggin and calculator

---
+++ src/main/java/pl/springtutorial/springtutorial/CalculatorDTO.java
@@ -0,0 +1,31 @@
+package pl.springtutorial.springtutorial;
+
+public class CalculatorDTO {
+    private int a;
+    private int b;
+    private String znak;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String getZnak() {
+        return znak;
+    }
+
+    public void setZnak(String znak) {
+        this.znak = znak;
+    }
+}
---
+++ src/main/java/pl/springtutorial/springtutorial/UserLoginDTO.java
@@ -0,0 +1,23 @@
+package pl.springtutorial.springtutorial;
+
+public class UserLoginDTO {
+
+    private String username;
+    private String passwd;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
